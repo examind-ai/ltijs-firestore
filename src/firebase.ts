67984a81779716c20fc3bbc41184946a02fc74d6
Johnny Oshika <johnnyoshika@gmail.com>
2022-07-30 23:08:41 -0700

    Provide a named instance to firebase-admin so mulitple apps can be initialized
    Relates to #3

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,11 @@
 import * as admin from 'firebase-admin';
 
 // initializeApp looks for service account credentials in GOOGLE_APPLICATION_CREDENTIALS environment variable
-// and determines which Firebase project we're using
-admin.initializeApp();
+// and determines which Firebase project we're using.
+// Provide a named instance so multiple apps can be initialized: https://github.com/examind-ai/ltijs-firestore/issues/3
+const app = admin.initializeApp(undefined, 'ltijs-firestore');
 
-const db = admin.firestore();
+const db = app.firestore();
 
 db.settings({ ignoreUndefinedProperties: true });
 
